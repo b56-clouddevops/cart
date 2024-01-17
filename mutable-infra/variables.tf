a1d99b0d23f9c07df7c930cf92e4875c00fd8424
CodingManoj <CodingManoj@gmail.com>
2024-01-16 21:09:04 -0500

    TF ALB Auto Push

--- mutable-infra/variables.tf
+++ mutable-infra/variables.tf
@@ -1,26 +1,8 @@
 variable "ENV" {}
 
-# Document DB Variables
-variable "DOCDB_PORT" {}
-variable "DOCDB_INSTANCE_TYPE" {}
-variable "DOCDB_INSTANCE_COUNT" {}
-variable "DOCDB_ENGINE" {}
+variable "COMPONENT" {}
+variable "SPOT_INSTANCE_TYPE" {}
+variable "SPOT_INSTANCE_COUNT" {}
 
-# MYSQL DB Variables
-variable "MYSQL_PORT" {}
-variable "MYSQL_FAMILY" {}
-variable "MYSQL_STORAGE" {}
-variable "MYSQL_ENGINE" {}
-variable "MYSQL_ENGINE_VERSION" {}
-variable "MYSQL_ENGINE_TYPE" {}
-
-# REDIS Variables
-variable "REDIS_PORT" {}
-variable "REDIS_ENGINE" {}
-variable "REDIS_ENGINE_FAMILY" {}
-variable "REDIS_ENGINE_VERSION" {}
-variable "REDIS_INSTANCE_COUNT" {}
-
-# RabbitMQ Variables
-variable "RABBITMQ_PORT_NUMBER" {}
-variable "RABBITMQ_INSTANCE_TYPE" {}
+variable "OD_INSTANCE_TYPE" {}
+variable "OD_INSTANCE_COUNT" {}
