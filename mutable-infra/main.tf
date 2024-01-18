e5df7a06ed638555e7b6c7969580ddd3b54ca046
CodingManoj <CodingManoj@gmail.com>
2024-01-17 20:25:12 -0500

    TF Backend Infra Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -2,8 +2,8 @@ module "cart" {
   source                = "./vendor/modules/app"
 
   ENV                   = var.ENV
-  APP_VERSION           = var.APP_VERSION
   COMPONENT             = var.COMPONENT
+  APP_VERSION           = var.APP_VERSION
   SPOT_INSTANCE_TYPE    = var.SPOT_INSTANCE_TYPE
   SPOT_INSTANCE_COUNT   = var.SPOT_INSTANCE_COUNT
   OD_INSTANCE_TYPE      = var.OD_INSTANCE_TYPE
